4a69b10f94d3b299ef426664b4fc4cffec7a1f4e
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-29 00:28:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,11 @@
 def key_for_min_value(name_hash)
   name = name_hash.first.first  
   smallest_val = name_hash.first.last 
+  name_hash.each do |k, v| 
+    if smallest_val > v 
+      name = k
+      smallest_val = v
+    end 
+  end 
+  name 
 end
